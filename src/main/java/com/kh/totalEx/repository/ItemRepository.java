7712104026ff5b76a366b60b2349b60d0c7ce66a
kimfjd <kimfjd@naver.com>
2024-06-03 16:43:44 +0900

    연습

---
+++ src/main/java/com/kh/totalEx/repository/ItemRepository.java
@@ -0,0 +1,8 @@
+package com.kh.totalEx.repository;
+
+import com.kh.totalEx.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+}
